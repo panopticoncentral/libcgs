cadf351adef22bb61b293e693a2c5d6b8ec14246
Paul Vick <paulv@microsoft.com>
2019-06-10 08:54:59 -0700

    Add terminal font

---
+++ src/libcgs/Console.cs
@@ -0,0 +1,18 @@
+﻿using SdlSharp;
+using SdlSharp.Graphics;
+
+namespace Citadel
+{
+    public sealed class Console
+    {
+        public Console()
+        {
+            var window = Window.Create((640, 480), WindowFlags.Shown, out var renderer);
+            var fontSurface = Image.LoadPng(RWOps.CreateReadOnly(Resources.terminal));
+            var fontTexture = renderer.CreateTexture(fontSurface);
+            renderer.Copy(fontTexture);
+            renderer.Present();
+            Timer.Delay(10000);
+        }
+    }
+}
